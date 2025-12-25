159be31b1814dc947b2dccc5b8143e7ff2017f29
cacoch <15091952+cacoch@users.noreply.github.com>
2025-12-25 01:20:46 +0100

    * disutil.core is deprecated * some formating

--- python/setup.py
+++ python/setup.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 
-from distutils.core import setup, Extension, extension_keywords
+from setuptools import setup, Extension
 
 #print extension_keywords
 
-module1 = Extension('krui',
-                    sources = ['snns/krui.c'],
-		    include_dirs = ['../kernel/sources'],
-		    library_dirs = ['../kernel/sources'],
-		    libraries = ['kernel','func','fl'])
+module1 = Extension(
+        'krui',
+        sources = ['snns/krui.c'],
+		include_dirs = ['../kernel/sources'],
+        extra_compile_args=['-std=c99'],
+		library_dirs = ['../kernel/sources'],
+		libraries = ['kernel','func','fl']
+        )
 
-setup (name = 'snns-kernel',
+setup (
+       name = 'snns-kernel',
        ext_package='snns',
        version = '0.1',
        author='Patrick Kursawe',
@@ -18,5 +22,6 @@ setup (name = 'snns-kernel',
        description = 'SNNS kernel functions',
        packages = ['snns'],
        ext_modules = [module1],
-       py_modules = ['snns.util'])
+       py_modules = ['snns.util']
+       )
 
